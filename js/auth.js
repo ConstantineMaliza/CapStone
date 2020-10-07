c6c02b026f0363c9a8ff77e78aac2bae26dcecfb
maliza constantine <malizacoco4@gmail.com>
2020-10-07 21:42:26 +0200

    to display block of username and password

--- js/auth.js
+++ js/auth.js
@@ -3,10 +3,16 @@
 auth.onAuthStateChanged(user =>{
     if(user){
         console.log('User Logged in ',user);
+
         document.getElementById('commentsdiv').style.display='block';
+        document.getElementById('username').style.display='block';
+        document.getElementById('logout').style.display='block';
         
     }else{
         console.log('User Logged out ');
+        document.getElementById('username').style.display='none';
+        document.getElementById('logout').style.display='none';
+        document.getElementById('commentsdiv').style.display='none';
     }
     
 });
