13479a7cff906c260ac1304c7d52f068ad01173f
maliza constantine <malizacoco4@gmail.com>
2020-10-07 23:03:54 +0200

    to keep session user for current user

--- js/auth.js
+++ js/auth.js
@@ -3,18 +3,33 @@
 auth.onAuthStateChanged(user =>{
     if(user){
         console.log('User Logged in ',user);
-
+       
         document.getElementById('commentsdiv').style.display='block';
         document.getElementById('username').style.display='block';
         document.getElementById('logout').style.display='block';
         
+       
+
     }else{
         console.log('User Logged out ');
         document.getElementById('username').style.display='none';
         document.getElementById('logout').style.display='none';
-        document.getElementById('commentsdiv').style.display='none';
+       
     }
     
 });
+
+//logout
+const logout = document.querySelector("#logout");
+logout.addEventListener('click',(e) =>{
+    e.preventDefault();
+
+    auth.signOut().then(()=>{
+         console.log('user signed out');
+    })
+});
+
+
+document.getElementById('username').innerHTML= sessionStorage.getItem('username');
 
 
