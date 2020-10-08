f9d71e5a8979233d19368c9a50c0627978f5d847
maliza constantine <malizacoco4@gmail.com>
2020-10-08 08:29:42 +0200

    retrieve all comment made on article

--- js/readarticle.js
+++ js/readarticle.js
@@ -3,16 +3,16 @@ function getUrlParameter(name) {
     var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
     var results = regex.exec(location.search);
     return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-  }
-  
-    var uid = getUrlParameter("postuid");
+}
 
-    db.collection("Blog").doc(uid).get().then(function(snapshot) {
+var uid = getUrlParameter("postuid");
 
-        var html="";
-        var childData = snapshot.data();
+db.collection("Blog").doc(uid).get().then(function (snapshot) {
 
-        html+=`
+    var html = "";
+    var childData = snapshot.data();
+
+    html += `
         <img src="${childData.photo}" alt="">
         <div class=""> 
             <div class="description">
@@ -21,32 +21,52 @@ function getUrlParameter(name) {
             </div>                      
         </div>
         `;
-        document.getElementById('article').innerHTML=html;
+    document.getElementById('article').innerHTML = html;
 
-           // leave comment
-const commentForm = document.querySelector('.commentForm');
+    // leave comment
+    const commentForm = document.querySelector('.commentForm');
 
-commentForm.addEventListener('submit',(e)=>{
-    e.preventDefault();
+    commentForm.addEventListener('submit', (e) => {
+        e.preventDefault();
 
-    const commentmessage = commentForm['commentmessage'].value;
-    const name = sessionStorage.getItem('username');
-    const blogtitle = childData.title;
+        const commentmessage = commentForm['commentmessage'].value;
+        const name = sessionStorage.getItem('username');
+        const blogtitle = childData.title;
 
-    var commentData ={
-        commentmessage,
-        name,
-        blogtitle,
-        date:new Date().getTime()
-    }
+        var commentData = {
+            commentmessage,
+            name,
+            blogtitle,
+            date: new Date().getTime()
+        }
 
-    db.collection("Comment").doc().set(commentData).catch(function(error) {
-        console.error("Error adding document: ", error);
-    }).then(function(){
-        alert('successful comment sent!');
-        // window.location.href="index.html";
+        db.collection("Comment").doc().set(commentData).catch(function (error) {
+            console.error("Error adding document: ", error);
+        }).then(function () {
+            alert('successful comment sent!');
+            // window.location.href="index.html";
+        });
     });
 });
-    });
 
- 
+db.collection("Comment").get().then(function(snapshot){
+    var html="";
+    snapshot.forEach(function(doc) {
+        var childData = doc.data();
+
+        html+=`
+        <div class="col-2 col-s-2">
+            <img src="assets/Ellipse 5.png" alt="">
+        </div>
+        <div class="col-10 col-s-10">
+            <div class="usercomments">
+                <h5>${childData.name}</h5>
+                <p>
+                    " ${childData.commentmessage} "
+                </p>
+            </div>
+        </div>
+        `;
+        document.getElementById('readcomment').innerHTML=html;
+    });
+})
