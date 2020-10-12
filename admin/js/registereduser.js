54bdc99a0135d5149744055e3211307ca89787c9
maliza constantine <malizacoco4@gmail.com>
2020-10-12 11:03:37 +0200

    changed

--- admin/js/registereduser.js
+++ admin/js/registereduser.js
@@ -1,22 +1,25 @@
 
 
-db.collection('RegisteredUser').get().then(function(snapshot){
+db.collection('RegisteredUser').get().then(function (snapshot) {
 
     var htmlAll = "";
     var childCounts = snapshot.size;
     var t = 0;
     var i = childCounts + 1;
     console.log(childCounts);
-    snapshot.forEach(function(doc) {
+    snapshot.forEach(function (doc) {
         var childUID = doc.id;
         var childData = doc.data();
-        if(t==0){
-            htmlAll = "";  
-          }
-          t++;
-          i--;
-        var html="";
-        html+=`
+        if (t == 0) {
+            htmlAll = "";
+        }
+        t++;
+        i--;
+        var html = "";
+        // if(childData.Role ==="admin"){
+        //     document.getElementById("admin").style.color= "green";
+        // }
+        html += `
         <tr>
             <td>${i}</td>
             <td><img src="${childData.photo}" alt="" srcset=""></td>
@@ -24,14 +27,15 @@ db.collection('RegisteredUser').get().then(function(snapshot){
             <td>${childData.Email}</td>
             <td>${childData.Role}</td>
             <td>
-                <a href="#deleteUser" class="icon" onclick="deleteUser('${childUID}')"><i class="fas fa-user-tie"></i></a>
-                <a href="#deleteUser" class="icon" onclick="promoteUser('${childUID}')"><i class="fas fa-trash-alt"></i></a>
+                <a href="#User" id="admin" class="icon" onclick="promoteUser(this, 'red')"><i class="fas fa-user-tie"></i></a>
+                <a href="#deleteUser" class="icon" onclick="deleteUser('${childUID}')"><i class="fas fa-trash-alt"></i></a>
+              
             </td>
         </tr>     
         `;
         htmlAll = html + htmlAll;
-        if(t == childCounts) {
-        document.getElementById("users").innerHTML=htmlAll ;
+        if (t == childCounts) {
+            document.getElementById("users").innerHTML = htmlAll;
 
 
         }
@@ -39,22 +43,33 @@ db.collection('RegisteredUser').get().then(function(snapshot){
     });
 });
 
-function deleteUser(childUID) {
-    
-    db.collection("RegisteredUser").doc(childUID).delete().then(function() {
-    
-    console.log("sucessfully deleted");
+function deleteUser(childUID, user_id) {
 
-    var element = document.getElementById(childUID);
+    db.collection("RegisteredUser").doc(user_id).delete().then(function () {
 
-    document.querySelector("#users").removeChild(element);
+        alert("sucessfully deleted");
+
+        var element = document.getElementById(childUID);
+
+        document.querySelector("#users").removeChild(element);
 
     });
 
 }
+function promoteUser(elmnt, clr) {
+    elmnt.style.color = clr;
+
+    // db.collection("RegisteredUser").get().then(function (snapshot) {
+    //     snapshot.forEach(function (doc) {
+    //         var childData = doc.data();
+    //         console.log(childData.Role);
+    //         document.getElementById("admin").style.color = "red";
+    //     });
+    // });
+}
 /// count 
 
-db.collection('RegisteredUser').get().then(function(snapshot){
+db.collection('RegisteredUser').get().then(function (snapshot) {
     var childCounts = snapshot.size;
-    document.getElementById("countuser").innerHTML=childCounts;
+    document.getElementById("countuser").innerHTML = childCounts;
 });
